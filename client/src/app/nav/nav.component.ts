e9ec8853d8853bf8240b2a94822e34d18ccd3247
Roel Verheyen <roel.verheyen@theuma.com>
2021-02-13 16:24:07 +0100

    End of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -10,36 +12,24 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {}
-  //loggedIn: boolean;
-  //currentUser$: Observable<User>;
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, 
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
-    //this.getCurrentUser();
-    //this.currentUser$ = this.AccountService.currentUser$;
   }
 
   login(){
     this.accountService.login(this.model).subscribe(Response => {
-      console.log(Response);
-      //this.loggedIn = true;
+      this.router.navigateByUrl('/members');
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
   logout(){
     this.accountService.logout();
-    //this.loggedIn = false;
+    this.router.navigateByUrl('/');
   }
 
-  /*
-  getCurrentUser(){
-    this.AccountService.currentUser$.subscribe(user => {
-      this.loggedIn = !!user; // !! boolean (if user = null, then false, alse true)
-    }, error => {
-      console.log(error);
-    })
-  }
-  */
 }
